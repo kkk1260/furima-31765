ffa3c8fd23af958bbeb0aac18b7b5e67e0b238fd
kkk1260 <n.k_02161999@ezweb.ne.jp>
2020-11-12 23:36:22 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,   presence: true, length: { maximum: 40 }
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_kata, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name,  presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_kata,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday,   presence: true
 end
