57c2be7f3fb49d806050266b86b2b9a1dbd2520c
kkk1260 <n.k_02161999@ezweb.ne.jp>
2020-11-18 11:48:29 +0900

    商品出品機能の追加実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -30,10 +30,22 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
 
+    it "cotegory_idが空の場合は出品できない" do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+    end
+
     it "商品の状態の指定がないと出品できない" do
       @item.condition_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition must be other than 1")
+    end
+
+    it "condition_idが空の場合は出品できない" do
+      @item.condition_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition can't be blank", "Condition is not a number")
     end
 
     it "配送料の負担について情報がないと出品できない" do
@@ -41,11 +53,35 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery price must be other than 1")
     end
+    
+    it "delivery_price_idが空の場合は出品できない" do
+      @item.delivery_price_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery price can't be blank", "Delivery price is not a number")
+    end
+
+    it "発送元の地域についての情報がないと出品できない" do
+      @item.shipment_source_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipment source must be other than 1")
+    end
+
+    it "shipment_source_idが空の場合は出品できない" do
+      @item.shipment_source_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipment source can't be blank", "Shipment source is not a number")
+    end
 
     it "発送までの日数について情報がないと出品できない" do
       @item.days_to_ship_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include( "Days to ship must be other than 1")
+    end
+
+    it "days_to_ship_idが空の場合は出品できない" do
+      @item.days_to_ship_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Days to ship can't be blank", "Days to ship is not a number")
     end
 
     it "価格について情報がないと出品できない" do
@@ -71,6 +107,8 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is not a number")
     end
+
+    
 
 
 
