76099f0dbd79158762e1ccee976aeb668aaf621c
kkk1260 <n.k_02161999@ezweb.ne.jp>
2020-11-20 13:47:34 +0900

    商品情報編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new, :create, :edit]
-
+  before_action :set_item, only: [:update, :edit, :show]
   def index
     @item = Item.all.order("created_at DESC")
   end
@@ -19,20 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if @item.user_id == current_user.id
-      render :edit
-    else
+    unless @item.user_id == current_user.id
       redirect_to root_path 
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,6 +40,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :condition_id, :price, :category_id, :delivery_price_id, 
                                  :shipment_source_id, :information, :days_to_ship_id ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
